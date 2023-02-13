5031544ede4f51d98c791f082571a857d9ef2641
lukling0524 <59614692+lukling0524@users.noreply.github.com>
2023-02-14 00:16:10 +0900

    비디오 -> 이미지 시퀀스로 변경

--- js/custom.js
+++ js/custom.js
@@ -16,47 +16,47 @@ document.documentElement.style.setProperty('--vh', `${vh}px`); // [2]
 /**
 * 초기화면 로딩시
 */
-setTimeout(function () {
-    $wrap.classList.remove('loading');
-    $scrollText.classList.add('show');
-}, 6600)
+// setTimeout(function () {
+//     $wrap.classList.remove('loading');
+//     $scrollText.classList.add('show');
+// }, 6600)
 
 
 /**
  * LOGO 눈 path 상하 움직임 animation 
  */
-setTimeout(function () {
-    $eye_l.classList.add('action');
-    $eye_r.classList.add('action');
-}, 6000)
+// setTimeout(function () {
+//     $eye_l.classList.add('action');
+//     $eye_r.classList.add('action');
+// }, 6000)
 
 
 /**
  * 로고 클릭 시, 새로고침 시 scrollY = 0
  */
-$scrollTop.addEventListener('click', function () {
-    window.scroll({ top: 0, behavior: 'smooth' });
-});
-
-window.onbeforeunload = function () {
-    window.scrollTo(0, 0);
-};
+// $scrollTop.addEventListener('click', function () {
+//     window.scroll({ top: 0, behavior: 'smooth' });
+// });
+// 
+// window.onbeforeunload = function () {
+//     window.scrollTo(0, 0);
+// };
 
 
 /**
  * 라이트모드-다크모드 토글 버튼
  */
-const $toggleBtn = document.querySelector('.toggle__item'),
-    $themeIcons = document.querySelectorAll('.toggle__icon .item');
-
-$toggleBtn.addEventListener('click', function () {
-    $html.classList.toggle('light-theme');
-
-    for (let i = 0; i < $themeIcons.length; i++) {
-        $themeIcons[i].classList.toggle('on');
-
-    }
-})
+// const $toggleBtn = document.querySelector('.toggle__item'),
+//     $themeIcons = document.querySelectorAll('.toggle__icon .item');
+// 
+// $toggleBtn.addEventListener('click', function () {
+//     $html.classList.toggle('light-theme');
+// 
+//     for (let i = 0; i < $themeIcons.length; i++) {
+//         $themeIcons[i].classList.toggle('on');
+// 
+//     }
+// })
 
 
 
@@ -165,41 +165,46 @@ new ScrollMagic.Scene({
 // });
 
 
-// About section 아바타 비디오 scroll 모션
-const $video = document.querySelector('.avata');
+// About section 아바타 이미지 시퀀스
+const avataImages = new Array();
+const imgTag = document.querySelector('.avata__sequence');
 
-let $videoScroll = new ScrollMagic.Scene({
+for (let i = 1; i < 48; i++) {
+    avataImages.push(`./images/imagesequence/avata-${i}.png`);
+}
+
+const img = { crntImg: 0 };
+
+let imageSequence = TweenMax.to(img, 0.5,
+    {
+        crntImg: avataImages.length - 1,
+        roundProps: 'crntImg',
+        immediateRender: true,
+        onUpdate: function () {
+            imgTag.setAttribute('src', avataImages[img.crntImg])
+        }
+    }
+);
+
+
+new ScrollMagic.Scene({
     duration: 3000,
     triggerElement: '.about',
     triggerHook: 0,
     offset: 100,
 })
+    .setTween(imageSequence)
     .setPin('.about')
     .addTo($controller)
-// .addIndicators({
-//     indent: 50,
-//     name: '비디오 비디오',
-//     colorStart: 'black',
-//     colorEnd: 'black',
-//     colorTrigger: 'black',
-// });
+    .addIndicators({
+        name: '이미지시퀀스',
+        colorStart: 'red',
+        colorEnd: 'red',
+        indent: 10
+    });
 
 
-//Video Animation
-let $accelamount = 0.1;
-let $scrollpos = 0;
-let $delay = 0;
 
-$videoScroll.on('update', e => {
-    if (e.scrollPos > e.startPos) {
-        $scrollpos = e.scrollPos / 1000;
-    }
-});
-
-setInterval(() => {
-    $delay += ($scrollpos - $delay) * $accelamount;
-    $video.currentTime = $scrollpos;
-}, 233.1);
 
 
 // about 텍스트 slide up
@@ -326,3 +331,57 @@ window.addEventListener("resize", function () {
     width = box.innerWidth;
     height = box.innerHeight;
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 
+//     var dataImages = new Array();
+// 
+// for (var i = 1; i < 92; i++) {
+//     dataImages.push('./img/data-' + i + '.jpg');
+// }
+// 
+// var dataObj = { curImg: 0 };
+// 
+// var tween = TweenMax.to(dataObj, 0.5,
+//     {
+//         curImg: dataImages.length - 1,
+//         roundProps: "curImg",
+//         immediateRender: true,
+//         onUpdate: function () {
+//             $("#data-logger").attr("src", dataImages[dataObj.curImg]);
+//         }
+//     }
+// );
+// 
+// //init controller
+// var controller = new ScrollMagic.Controller();
+// 
+// var dataLogger = new ScrollMagic.Scene({
+//     triggerElement: "#trigger-target",
+//     duration: 6000,
+//     triggerHook: 0
+// })
+//     .setPin("#trigger-target")
+//     .setTween(tween)
+//     .addIndicators({
+//         name: '-PICPICPIC',
+//         colorStart: 'red',
+//         colorEnd: 'red',
+//         indent: 10
+//     })
+//     .addTo(controller);
+// 
